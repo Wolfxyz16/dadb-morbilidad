ff7230752a089aac645aa0849445d4b6a25e163e
Wolfxyz16 <yerayliloaiza@gmail.com>
2025-05-06 20:11:32 +0200

    deleted this folder, idk how it ended up here

--- dadb-morbilidad/compress_and_send.sh
+++
@@ -1,8 +0,0 @@
-# Comprimimos el projecto
-zip -q -r project.zip .
-
-# Lo enviamos al puerto 1234 del host
-nc -v 10.245.169.129 1234 < project.zip
-
-# Eliminamos el archivo
-rm project.zip
--- dadb-morbilidad/ejercicio1/download-data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "downloading data from ine"
-
-wget --output-document=./viviendas.csv https://www.ine.es/jaxi/files/tpx/csv_bdsc/59531.csv --progress=dot:binary --no-verbose --show-progress
-
-echo "done!"
