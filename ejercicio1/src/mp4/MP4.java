3025554232e0ebe5481857f3862dbbc62c09fd9a
Wolfxyz16 <yerayliloaiza@gmail.com>
2025-05-06 20:08:41 +0200

    adapted mp4

--- ejercicio1/src/mp4/MP4.java
+++ ejercicio1/src/mp4/MP4.java
@@ -17,66 +17,78 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 
-public class MP1 {
+public class MP4 {
 
-	public static class MP1Mapper extends Mapper<LongWritable, Text, Text, Text> {
+	public static class MP4Mapper extends Mapper<LongWritable, Text, Text, Text> {
 
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-			
-			// Find the town with the highest percentage of empty housing
-			// e.g. (Dima_t, 855)
-			// e.g. (Dima_e, 117)
-			
-			String line = value.toString();
-			String[] fields = line.split(";");
+      // Separar las columnas
+      String line = value.toString();
+      String[] columnas = line.split(";");
 
-			// We get the first char of the second word
-			// 't' or 'v' stands for totales and vacias
-			char type = fields[1].split(" ")[1].charAt(0);
-			
-			// First we must check that "Consumo electrico" is "totales" or "vacias"
-			if( type != 't' && type != 'v' ) return;
+      // Coger el valor del código postal si es que lo hay
+      String codigo_postal = columnas[0].split(" ")[0];
 
-			// Now we remove the dots from the String that represents a number, 1.285 => 1285
-			fields[2] = fields[2].replace(".", "");
+      // Comprobar que es el valor del código postal
+      try {
+          int numero_provincia = Integer.parseInt(codigo_postal);
+      } catch (NumberFormatException e) {
+          return;
+      } catch (IndexOutOfBoundsException e) {
+          System.out.println("Error: la lista está vacía.");
+      }
 
-			// 	We get:
-			//		* first number that appears on the line (code)
-			//		* town name
-			//		* the number of houses (third field)
-			String[] code_plus_town = fields[0].split(" ");
-			String code = code_plus_town[0];
-			String town = code_plus_town[1];
-			int houses = Integer.parseInt(fields[2]);
+      // Comprobar si el código postal es válido
+      if (numero_provincia / 1000 == 0) return;
+      
+      // Pasar del código postal del municipio al de la provincia
+      numero_provincia = numero_provincia/1000
+      
+      // Añadir un 0 si es necesario (En realidad esto solo es para que quede bonito)
+      String clave;
+      if (numero_provincia < 10){
+          clave = String.format("%02d", numero_provincia);
+      } else{
+          clave = String.valueOf(numero_provincia);
+      }
+      
+      // Comprobar que estamos en un elemento que muestra el consumo eléctrico
+      // Si estamos en el consumo eléctrico coger los varemos de electricidad
+      String tipo = columnas.get(1);
+      int electricidad_total;
+      if(tipo.contains("Viviendas con cosumo entre")){
+          String[] palabras_tipo = tipo.split(" ");
+          int elec1 = palabras_tipo[4].replace(".", "");
+          int elec2 = palabras_tipo[6].replace(".", "");
+          
+          
+      } else return;
+
+      //Conseguir el número de casas
+      try {
+          int num_casas = Integer.parseInt(columnas[2].replace(".", ""))
+          } catch (NumberFormatException e) {
+              return;
+          }
+      
+      // Calcular electricidad total
+      electricidad_total = num_casas*(elec1+elec2)/2
+      context.write(new Text(clave), new IntWritable(electricidad_total));      
 			
-			// Code must be bigger than two in order to be a town, not a "comunidad"
-			if( code.length() > 2 ) {
-				context.write(
-						new Text(town),
-						new Text(String.valueOf(houses) + '_' + type)
-					);
-			}
 		}
 	}
 	
-	public static class MP1Reducer extends Reducer<Text, Text, Text, DoubleWritable> {
+	public static class MP4Reducer extends Reducer<Text, Text, Text, DoubleWritable> {
 
 		public void reduce(Text townName, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-
-			int vacias = 0, totales = 1;
-			
-			for( Text value : values ) {
-				// We split the values in number of houses and type (vacias or totales)
-				String[] parts = value.toString().split("_");
-				int numHouses = Integer.parseInt(parts[0]);
-				char type = parts[1].charAt(0);
-				
-				// if the type match we store the houses number
-				if( type == 'v' ) vacias = numHouses;
-				else if ( type == 't' ) totales = numHouses;
-			}
-			
-			context.write(townName, new DoubleWritable(Double.valueOf(vacias) / Double.valueOf(totales)));
+      int count = 0;
+      //Contar el numero de casas con mismo código postal
+      while (value.hasNext())
+      {
+          IntWritable i = value.next();
+          count += i.get();
+      }
+      context.write(key, new IntWritable(count));
 		}
 	}
 
@@ -89,9 +101,9 @@ public class MP1 {
 		Configuration conf = new Configuration();
 		Job job = Job.getInstance(conf, "Municipios con mayor porcentaje de viviendas vacias");
 		
-		job.setJarByClass(MP1.class);
-		job.setMapperClass(MP1Mapper.class);
-		job.setReducerClass(MP1Reducer.class);
+		job.setJarByClass(MP4.class);
+		job.setMapperClass(MP4Mapper.class);
+		job.setReducerClass(MP4Reducer.class);
 		
 		job.setMapOutputKeyClass(Text.class);
 		job.setMapOutputValueClass(Text.class);
